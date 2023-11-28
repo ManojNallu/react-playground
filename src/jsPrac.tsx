b7fd508548d436f946fd01a42d31c0c17c53ae38
ManojNallu <nmanoj1716@gmail.com>
2023-11-28 16:03:05 +0530

    jsprac added

---
+++ src/jsPrac.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+const MyFunction: React.FC = () => {
+
+    return (
+        <>
+            <p>hello</p>
+            {console.log("manoj")}
+        </>
+
+    );
+}
+
+export default MyFunction;
